822b606009a0b721d5b39d7cab3dd31bf869a261
harmansaini4tech <harmansaini10000@gmail.com>
2024-11-25 17:31:00 +0530

    array methods

---
+++ DAY 7 25-11-24/index.js
@@ -0,0 +1,84 @@
+// push 
+// let arr = [8,4,"d",7,5,"z"]
+// let pushed = arr.push(1,2,3)
+// console.log(pushed,arr)
+
+// pop
+// let arr = [8,4,"d",7,5,"z",]
+// let poped = arr.pop()
+// console.log(poped,arr)
+
+
+// //map 
+// let arr = [8,4,"d",7,5,"z"]
+// let maped = arr.map(elem =>  {
+//     if(typeof elem === "number")
+//     return elem
+// } )
+// console.log(maped,arr)
+
+// // forEach
+// let arr = [8,4,"d",7,5,"z"]
+// let maped = arr.forEach(elem =>  {
+//     if(typeof elem === "number")
+//     console.log(elem)
+// } )
+// console.log(maped,arr)
+
+
+// filter
+// let arr = [8,4,"d",7,5,"z"]
+// let filtered = arr.filter(elem =>  typeof elem === "number")
+// console.log(filtered,arr)
+
+//find 
+// let arr = ["a","b",8,4,"d",7,5,"z"]
+// let finded = arr.find(elem =>  typeof elem === "number")
+// console.log(finded,arr)
+
+//slice
+// let arr = ["a","b",8,4,"d",7,5,"z"]
+// let sliced = arr.slice(0,3)
+// console.log(sliced,arr)
+
+// //splice
+// let arr = ["a","b",8,4,"d",7,5,"z"]
+// let spliced = arr.splice(2,3,"harman")
+// console.log(spliced,arr)
+
+//shift
+// let arr = ["a","b",8,4,"d",7,5,"z"]
+// let shifted = arr.shift()
+// console.log(shifted,arr)
+
+// unshift
+// let arr = ["a","b",8,4,"d",7,5,"z"]
+// let unshifted = arr.unshift(6)
+// console.log(unshifted,arr)
+
+// split = not made for array. it works on string
+// let text = "hello world foo bar";
+// let result = text.split(" ");
+// console.log(result); 
+
+// join
+// let arr = ["i","am","harman"];
+// let result = arr.join("")
+// console.log(result); 
+
+// includes
+// let arr = ["i","am","harman"];
+// let result = arr.includes("harman")
+// console.log(result); 
+
+// //reduce
+// let arr = ["a","b",8,4,"d",2,2,"z"]
+// let str = ""
+// let reduced = arr.reduce((accum,curr)=>{
+//     if(typeof curr === "number")  {
+//         accum +=curr 
+//     }
+//     return accum 
+// },0)
+// console.log(reduced,str,arr)
+
